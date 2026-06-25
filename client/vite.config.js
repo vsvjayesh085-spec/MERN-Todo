be9fc1b7f9ade85703427c754e93aa4e03f1b698
vsvjayesh085-spec <vsvjayesh085@gmail.com>
2026-06-25 23:30:20 +0530

    U6y

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,3 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
++hattp_jjhhhhhh
+hjjjhhhhhh
+hiiuujjuuhhhggfffftrtdff
